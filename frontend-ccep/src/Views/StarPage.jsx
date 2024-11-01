d009a713634f5df94e3c9f8cac319790e5e83cc8
miguelpardo22 <miangelpardo123gmail.com>
2024-10-31 19:06:26 -0500

    Avances Dashboard

--- frontend-ccep/src/Views/StarPage.jsx
+++ frontend-ccep/src/Views/StarPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 function StartPage() {
   return (
-    <h1>StartPage</h1>
-  )
+    <div>
+      <h1>StartPage</h1>
+      <div className="card-container">
+        {/* Cards */}
+        <div className="card">Productos Vendidos: </div>
+        <div className="card">Numero de Ventas:</div>
+        <div className="card">Valor Total Ventas:</div>
+        <div className="card">Numero de Compras: </div>
+        <div className="card">Card 5</div>
+        <div className="card">Card 6</div>
+        <div className="card">Card 7</div>
+        <div className="card">Card 8</div>
+        {/* Añade más cards según sea necesario */}
+      </div>
+      <div className="additional-content">
+        {/* Contenido adicional */}
+        <p>Aquí puedes añadir un gráfico, una lista de tareas, o cualquier otro contenido relevante.</p>
+      </div>
+    </div>
+  );
 }
 
 export { StartPage };
