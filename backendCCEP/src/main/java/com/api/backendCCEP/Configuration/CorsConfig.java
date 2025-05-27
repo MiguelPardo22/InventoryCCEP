4a263e07e16a1762ad453463acf50b190b8e62ea
MiguelPardo22 <miangelpardo123@gmail.com>
2025-05-26 23:22:49 -0500

    Avances

--- backendCCEP/src/main/java/com/api/backendCCEP/Configuration/CorsConfig.java
+++ backendCCEP/src/main/java/com/api/backendCCEP/Configuration/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://inventory-ccep.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization");
